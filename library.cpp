61b732eb0f737eacd6f52fbf6e38b991ad931d76
TuckerSelf <tself3@ut.utm.edu>
2023-10-31 20:46:52 -0500

    humble beginnings

--- library.cpp
+++ library.cpp
@@ -15,12 +15,32 @@
 
 using namespace std;
 
-void library::push_front(string bookTitle, string authName, int pageNum, string isbnInsert, float cover, int yearNum){
+void library::read_file(string fileName){
+  ifstream inFile;
+  inFile.open(fileName);
+  while(inFile){
+    
+  }
+  inFile.close();
+}
 
+void library::write_file(string fileName){
+  ofstream outFile;
+  outFile.open(fileName);
+
+  outFile.close();
+}
+
+void library::push_front(string bookTitle, string authName, int pageNum, string isbnInsert, float cover, int yearNum){
+  
+  
+  entries.pushfront();
 }
 
 void library::push_back(string bookTitle, string authName, int pageNum, string isbnInsert, float cover, int yearNum){
 
+
+  entries.pushback();
 }
 
 void library::insert_sorted(string bookTitle, string authName, int pageNum, string isbnInsert, float cover, int yearNum){
@@ -28,17 +48,36 @@ void library::insert_sorted(string bookTitle, string authName, int pageNum, stri
 }
 
 string library::find_author(string authName){
+  list<book>::iterator it;
 
+  if(){
+
+  }
+  
 }
 
 string library::find_album(string bookTitle){
+  list<book>::iterator it;
 
+  if(){
+
+  }
+  
 }
 
 void library::delete(string authName, string bookName){
+  list<book>::iterator it;
 
+  if(){
+    if(){
+
+    }
+  }
+
+  entries.erase();
 }
 
 void library::print_list(){
-
+  list<book>::iterator it;
+  
 }
